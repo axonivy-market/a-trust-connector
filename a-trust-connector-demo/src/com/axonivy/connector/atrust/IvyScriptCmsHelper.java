4374374a9176c645eeb4b3da84b8008a9d5a9e7a
Jacek <jacek.lajdecki@axonivy.com>
2022-09-15 17:01:39 +0200

    XIVY-9116 Move class to package, adjust names

--- a-trust-connector-demo/src/com/axonivy/connector/atrust/IvyScriptCmsHelper.java
+++ a-trust-connector-demo/src/com/axonivy/connector/atrust/IvyScriptCmsHelper.java
@@ -1,9 +1,10 @@
+package com.axonivy.connector.atrust;
 import ch.ivyteam.ivy.cm.ContentObjectValue;
 import ch.ivyteam.ivy.environment.Ivy;
 
-public class ProcessHelper {
+public class IvyScriptCmsHelper {
 
-  public static ContentObjectValue getTrustTestDoc(String path) {
+  public static ContentObjectValue getContentObjectValue(String path) {
     return Ivy.cms().get(path).get().value().get();
   }
 
