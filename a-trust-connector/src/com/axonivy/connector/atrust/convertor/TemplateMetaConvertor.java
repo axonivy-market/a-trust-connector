c43f1605d62d8de67393bcd9899263639be24156
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2026-09-09 13:28:07 +0200

    fix: deprecated StringUtils usage

--- a-trust-connector/src/com/axonivy/connector/atrust/convertor/TemplateMetaConvertor.java
+++ a-trust-connector/src/com/axonivy/connector/atrust/convertor/TemplateMetaConvertor.java
@@ -1,17 +1,16 @@
 package com.axonivy.connector.atrust.convertor;
 
-import jakarta.faces.component.UIComponent;
-import jakarta.faces.context.FacesContext;
-import jakarta.faces.convert.Converter;
-import jakarta.faces.convert.ConverterException;
-import jakarta.faces.convert.FacesConverter;
-
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Strings;
 
 import com.axonivy.connector.atrust.service.ATrustService;
 
 import at.a.trust.rest.api.client.TemplateMeta;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.Converter;
+import jakarta.faces.convert.ConverterException;
+import jakarta.faces.convert.FacesConverter;
 
 @FacesConverter(value = "templateMetaConvertor", managed = true)
 @ApplicationScoped
@@ -20,7 +19,7 @@ public class TemplateMetaConvertor implements Converter<Object> {
 	@Override
 	public Object getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException {
 		return ATrustService.getAvailableTemplates().stream()
-				.filter(template -> StringUtils.equals(String.valueOf(template.getId()), value)).findAny().orElse(null);
+				.filter(template -> Strings.CS.equals(String.valueOf(template.getId()), value)).findAny().orElse(null);
 	}
 
 	@Override
